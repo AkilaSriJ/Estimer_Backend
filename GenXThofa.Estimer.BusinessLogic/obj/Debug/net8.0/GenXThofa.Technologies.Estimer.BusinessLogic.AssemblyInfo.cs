cbca91aea8c08bcfdcfb28c5da05a5770f4505df
AkilaSriJ <akila@genxthofa.com>
2025-12-22 10:46:05 +0530

    Add gitignore and remove Visual Studio files

--- GenXThofa.Estimer.BusinessLogic/obj/Debug/net8.0/GenXThofa.Technologies.Estimer.BusinessLogic.AssemblyInfo.cs
+++ GenXThofa.Estimer.BusinessLogic/obj/Debug/net8.0/GenXThofa.Technologies.Estimer.BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenXThofa.Technologies.Estimer.BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9462b3e2bfd5eda83785fc8ea83af45f0d21d3c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenXThofa.Technologies.Estimer.BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenXThofa.Technologies.Estimer.BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
